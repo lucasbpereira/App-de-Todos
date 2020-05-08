9c362a503595cc0ceb955159391e697f7be33cc7
Lucas Barbosa <lucasbpereira@outlook.com>
2020-05-08 16:49:58 -0300

    Criada a função addTodo
    criada a função para adicionar todos na lista [todos.js]

--- todos.js
+++ todos.js
@@ -10,6 +10,8 @@ var todos = [
 ]
 
 function renderTodos() {
+    listElement.innerHTML = '';
+
     for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
@@ -20,3 +22,13 @@ function renderTodos() {
 }
 
 renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = ('');
+    renderTodos();
+}
+
+btnElement.onclick = addTodo;
