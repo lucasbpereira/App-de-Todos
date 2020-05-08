54043f2c8cedc9430be3a9ae3cad49230e60cb7f
Lucas Barbosa <lucasbpereira@outlook.com>
2020-05-08 16:23:11 -0300

    Iniciando e selecionando DOM dos elementos.
    Selecionando os elementos da DOM com querySelector[index.html & todos.js]

--- todos.js
+++ todos.js
@@ -1,1 +1,10 @@
-alert('Funcionou!')
+var listElement = document.querySelector('#app li');
+var inputElement = document.querySelector('#app input');
+var btnElement = document.querySelector('#app button');
+var excluirlistElement = document.querySelector('excluir');
+
+var todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar site da Rocketseat'
+]
