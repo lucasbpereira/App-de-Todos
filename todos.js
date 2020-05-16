6ad053bcca42d1394b3e54b03cd034c155f0f833
Lucas Barbosa <lucasbpereira@outlook.com>
2020-05-16 16:39:00 -0300

    Estilização do documento
    Criação do documento style.css;
    Estilização do app;
    Inclusão de icones svg;
    Tentativa de implementar função

--- todos.js
+++ todos.js
@@ -12,18 +12,26 @@ function renderTodos() {
         var todoText = document.createTextNode(todo);
 
         var deleteElement = document.createElement('a');
-
         deleteElement.setAttribute('href', '#');
-
         var pos = todos.indexOf(todo);
         deleteElement.setAttribute('onclick', 'deleteTodo('+ pos +')');
+        var deleteImg = document.createElement('img');
+        deleteImg.setAttribute('src', '/svg/remove.svg')
+        deleteElement.appendChild(deleteImg);
 
-        var deleteText = document.createTextNode(' x');
+        var confirmElement = document.createElement('a');
+        confirmElement.setAttribute('href', '#');
+        confirmElement.setAttribute('onclick', 'confirmTodo('+ pos +')');
+        confirmElement.setAttribute('class', 'confirm');
+        var confirmImg = document.createElement('img');
+        confirmImg.setAttribute('src', '/svg/confirm.svg')
+        confirmElement.appendChild(confirmImg);
 
-        deleteElement.appendChild(deleteText);
+
 
         todoElement.appendChild(todoText);
         todoElement.appendChild(deleteElement);
+        todoElement.appendChild(confirmElement);
 
         listElement.appendChild(todoElement);
     }
@@ -44,6 +52,15 @@ btnElement.onclick = addTodo;
 
 function deleteTodo(pos) {
     todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function confirmTodo() {
+    const liElement = document.getElementsByTagName('li')[0];
+    const cfrmElement = document.getElementsByTagName('confirm')[0];
+    liElement.style.color = 'blue';
+    liElement.style.textAlign = 'justify';
     renderTodos();
     saveToStorage();
 }
